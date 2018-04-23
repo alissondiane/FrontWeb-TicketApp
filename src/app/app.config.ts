c5840b7dd9283f704c6d355238f7784c6bf11a61
Alisson <diane_10_96@hotmail.com>
2018-04-23 01:37:28 -0500

    Sprint2CrearMenu

--- src/app/app.config.ts
+++
@@ -1,4 +0,0 @@
-export class AppConfig {
-    public static SERVER_ADDRESS = 'https://modulo-alumno-zuul.herokuapp.com/modulo-alumno-client';
-}
-  
--- src/app/app.config.user.ts
+++
@@ -1,7 +0,0 @@
-import { AppConfig } from './app.config';
-
-export class UserConfig {
-  public static httpResource: any = {
-    solicitudes:  AppConfig.SERVER_ADDRESS + '/alumno/leer'
-  };
-}
